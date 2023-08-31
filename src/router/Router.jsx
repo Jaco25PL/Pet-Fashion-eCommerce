51887635b660474831c7c9299e20782eac0ac24e
PiedraL <joaquinlangone12@gmail.com>
2023-08-30 23:57:48 -0300

    routes up-to-date

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { NavBar } from "../components/NavBar/NavBar"
 import { ItemListContainer } from "../components/ItemListContainer/ItemListContainer"
 import { ItemDetailContainer } from "../components/ItemDetailContainer/ItemDetailContainer"
+import { Contact } from "../components/Contact/Contact"
+import { AboutUs } from "../components/AboutUs/AboutUs"
 
 export const Router = () => {
 
@@ -12,6 +14,8 @@ export const Router = () => {
             <Route path='/' element={<ItemListContainer/>}/>
             <Route path='/category/:id' element={<ItemListContainer/>}/>
             <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/ourStory" element={<AboutUs/>}/>
           </Routes>
         </BrowserRouter>
     )
