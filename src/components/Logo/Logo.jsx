e2241c767244461a6c00caab88b70d76ae142e60
PiedraL <joaquinlangone12@gmail.com>
2023-08-31 01:16:24 -0300

    changing the logo img name in Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom'
-import viteLogo from '../../assets/PetFamilyLogo.png'
+import brandLogo from '../../assets/PetFamilyLogo.png'
 import styles from "./styles.module.css"
 
 export const Logo = () => {
   return(  
         <Link to={"/"}>
-            <img className={styles.logo} src={viteLogo} alt="Vite logo" />
+            <img className={styles.logo} src={brandLogo} alt="Pet-Family" />
         </Link>
     )
 } 
