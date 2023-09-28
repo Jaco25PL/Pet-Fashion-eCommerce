dff3bdd20f98df59faffac01b0e9acdb89f71d22
PiedraL <joaquinlangone12@gmail.com>
2023-09-27 21:46:08 -0300

    mobile menu

--- src/components/NavBarCategories/NavBarCategories.jsx
+++ src/components/NavBarCategories/NavBarCategories.jsx
@@ -4,13 +4,16 @@ import styles from "./styles.module.css"
 export const NavBarCategories = () => {
 
     return (
-        <div className={styles.container}>
+        <div>
+            <label htmlFor="menu" className={styles.openMenu}><i className="bi bi-list"></i></label>
+            <input type="checkbox" id="menu"/>
             <ul className={styles.categoryContainer}>
                 <li><Link  className={styles.linkItem} to={"/"}>HOME</Link></li>
                 <li><Link  className={styles.linkItem} to={"/category/Wear"}>WEAR</Link></li>
                 <li><Link  className={styles.linkItem} to={"/category/Accessories"}>ACCESSORIES</Link></li>
                 <li><Link  className={styles.linkItem} to={"/ourStory"}>OUR STORY</Link></li>
             </ul>
+            <span className={styles.bgFilter}/>
         </div>
         
     )
